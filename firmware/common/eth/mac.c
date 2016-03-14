0a31f6211df5984691b7802424ab36aab3079dcc
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-03-14 11:51:30 +0100

    firmware: eth: add MAC generation for IO(DDR|ETH)1
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/eth/mac.c
+++ firmware/common/eth/mac.c
@@ -1,12 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <HAL/hal/hal.h>
+#include "internal/cnoc_debug.h"
 
+static int debug_cnoc_init = 0;
+void init_debug_cnoc()  {
+
+  //may be change by a real route finding function
+  unsigned int route = 0x2 | 0x2 << 3;
+  unsigned int return_route = 0x2 | 0x2 << 3;
+
+  mppa_syscnoc[0]->control.header.dword = 0xC0000000 | (route>>3);
+  mppa_syscnoc[0]->control.config.dword = route & 0x7; //fdir
+  // Set return route
+  __k1_cnoc_debug_send_set_return_route_first_dir (return_route & 0x7);
+  __k1_cnoc_debug_send_set_return_route (0xC0000000 | (return_route>>3));
+  __k1_cnoc_debug_send_set_ctrl_reg (1);
+}
 int mppa_ethernet_generate_mac(unsigned int ioeth_id, unsigned int ifce_id, uint8_t *buffer)
 {
-	uint64_t timestamp;
+	uint64_t serial, mac;
 	uint8_t ifce_value = 0;
 	int i;
+	const int clus_id = __k1_get_cluster_id();
 
 	if (ifce_id >= 4) {
 		fprintf(stderr, "[ETH] Error: Wrong interface id %d\n", ifce_id);
@@ -23,19 +39,31 @@ int mppa_ethernet_generate_mac(unsigned int ioeth_id, unsigned int ifce_id, uint
 		return -1;
 	}
 
-	if (__k1_get_cluster_id() != 128 && __k1_get_cluster_id() != 160) {
-		/* no fuses on this cluser */
-		fprintf(stderr, "[ETH] Warning: Serial number accessible only on IO0!\n");
-		timestamp = 0ULL;
-	} else {
-		uint64_t serial;
-
+	if ((clus_id >= 128 && clus_id <= 131) ||
+	    (clus_id >= 160 && clus_id <= 163)) {
 		mppa_fuse_init();
 		serial = mppa_fuse_get_serial();
-		if (serial == 0) {
-			fprintf(stderr, "[ETH] Warning: Fuse returns bad serial\n");
-		}
+	} else if ((clus_id >= 192 && clus_id <= 195) ||
+		   (clus_id >= 224 && clus_id <= 227)) {
 
+		if (!debug_cnoc_init) {
+			init_debug_cnoc();
+			debug_cnoc_init = 1;
+		}
+		serial = __k1_cnoc_debug_peek((unsigned int) &mppa_ftu[0]->fuse_box[1][0],
+					      sizeof(unsigned long long));
+
+	} else {
+		/* no fuses on this cluser */
+		fprintf(stderr, "[ETH] Warning: Could not access fuse\n");
+		serial = 0ULL;
+	}
+
+	if (serial == 0) {
+		fprintf(stderr, "[ETH] Warning: Fuse returns bad serial\n");
+	}
+	{
+		uint64_t timestamp;
 		unsigned year = (serial >> 36) & 0xff;
 		unsigned month = (serial >> 32) & 0x0f;
 		unsigned day = (serial >> 27) & 0x1f;
@@ -49,8 +77,8 @@ int mppa_ethernet_generate_mac(unsigned int ioeth_id, unsigned int ifce_id, uint
 			day * 24 * 60 +
 			hour * 60 +
 			minute;
+		mac = (0x02ULL << 40) | ((timestamp << 4) & 0xfffffffff0) | (ifce_value << 1);
 	}
-	uint64_t mac = (0x02ULL << 40) | ((timestamp << 4) & 0xfffffffff0) | (ifce_value << 1);
 	/* Generate the MAC */
 	for (i = 0; i < 6; i++) {
 		uint8_t v = (mac >> (i * 8)) & 0xff;
