3bfadb45280c78726e03caccffc4fc54069afe67
Jerome Reybert <jerome.reybert@kalray.eu>
2016-03-23 14:33:41 +0100

    firmware: common: netdev: remove unused linker script

--- firmware/common/netdev/include.mk
+++ firmware/common/netdev/include.mk
@@ -3,4 +3,4 @@ SRCDIRS   += $(NETDEVDIR)
 
 HDRFILES  += $(TOP_SRCDIR)/mppaeth/mppa_pcie_netdev.h
 _CFLAGS   += -I$(TOP_SRCDIR)/mppaeth
-_LDFLAGS  += -lpcie_queue -T$(NETDEVDIR)/linker.ld
+_LDFLAGS  += -lpcie_queue
