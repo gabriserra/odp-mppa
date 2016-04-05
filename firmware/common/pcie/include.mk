323838dde90845914e8de239586391b47cb5ab22
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-04-05 11:29:25 +0200

    firmware: pcie: autoload netdev module
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/pcie/include.mk
+++ firmware/common/pcie/include.mk
@@ -1,8 +1,12 @@
 ifndef __PCIE_INCLUDED__
 __PCIE_INCLUDED__ := 1
 
-SRCDIRS  += $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+PCIEDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+SRCDIRS  += $(PCIEDIR)
 _CFLAGS  += -I$(realpath $(K1_TOOLCHAIN_DIR))/../..//src/k1-mppapcie-dkms-2.2.0/mppapcie_netdev/
 _LDFLAGS += -Wl,--undefined=__pcie_rpc_constructor
+
+include $(PCIEDIR)/../netdev/include.mk
 
 endif
