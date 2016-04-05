0ea97cb8c7e4fc689f06963c9e3b4c39ce231b57
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-04-05 10:42:40 +0200

    firmware: netdev: remove dependency to previous netdev header file
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/netdev/include.mk
+++ firmware/common/netdev/include.mk
@@ -1,6 +1,5 @@
 NETDEVDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 SRCDIRS   += $(NETDEVDIR)
 
-HDRFILES  += $(TOP_SRCDIR)/mppaeth/mppa_pcie_netdev.h
 _CFLAGS   += -I$(TOP_SRCDIR)/mppaeth
 _LDFLAGS  += -lpcie_queue
