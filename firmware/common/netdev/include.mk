d1d8058e42946f8c69e4450ca26b8b4e0b04b0cb
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-04-05 11:17:05 +0200

    firmware: fix build without mppaeth locally
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/netdev/include.mk
+++ firmware/common/netdev/include.mk
@@ -1,5 +1,6 @@
 NETDEVDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 SRCDIRS   += $(NETDEVDIR)
 
+_CFLAGS   += -I$(realpath $(K1_TOOLCHAIN_DIR))/../..//src/k1-mppapcie-dkms-2.2.0/mppapcie_netdev/
 _CFLAGS   += -I$(TOP_SRCDIR)/mppaeth
 _LDFLAGS  += -lpcie_queue
