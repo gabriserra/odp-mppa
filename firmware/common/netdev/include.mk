60108c75775ad13c676d09385580774119bc6513
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-04-07 08:38:30 +0200

    firmware: netdev,pcie: support custom driver install dir
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/netdev/include.mk
+++ firmware/common/netdev/include.mk
@@ -4,7 +4,11 @@ __NETDEV_INCLUDED__ := 1
 NETDEVDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 SRCDIRS   += $(NETDEVDIR)
 
-_CFLAGS   += -I$(realpath $(K1_TOOLCHAIN_DIR))/../..//src/k1-mppapcie-dkms-2.2.0/mppapcie_netdev/
+ifdef MPPAPCIE_DIR
+_CFLAGS  += -I$(MPPAPCIE_DIR)/mppapcie_netdev/
+else
+_CFLAGS  += -I$(realpath $(K1_TOOLCHAIN_DIR))/../..//src/k1-mppapcie-dkms-2.2.0/mppapcie_netdev/
+endif
 _CFLAGS   += -I$(TOP_SRCDIR)/mppaeth
 _LDFLAGS  += -lpcie_queue
 
--- firmware/common/pcie/include.mk
+++ firmware/common/pcie/include.mk
@@ -4,7 +4,11 @@ __PCIE_INCLUDED__ := 1
 PCIEDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 
 SRCDIRS  += $(PCIEDIR)
+ifdef MPPAPCIE_DIR
+_CFLAGS  += -I$(MPPAPCIE_DIR)/mppapcie_netdev/
+else
 _CFLAGS  += -I$(realpath $(K1_TOOLCHAIN_DIR))/../..//src/k1-mppapcie-dkms-2.2.0/mppapcie_netdev/
+endif
 _LDFLAGS += -Wl,--undefined=__pcie_rpc_constructor
 
 include $(PCIEDIR)/../netdev/include.mk
