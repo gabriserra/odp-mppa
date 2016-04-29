eb712c4d1963ffe414fc4b5916e375a9fb717504
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-04-29 11:46:24 +0200

    mppapcie_odp: indent -kr -i8
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- mppapcie_odp/odp.h
+++ mppapcie_odp/odp.h
@@ -24,7 +24,7 @@ struct odp_tx {
 	struct scatterlist sg[MAX_SKB_FRAGS + 1];
 	u32 sg_len;
 	dma_cookie_t cookie;
-	u32 len; /* to be able to free the skb in the TX 2nd step */
+	u32 len;		/* to be able to free the skb in the TX 2nd step */
 	union mppa_timestamp time;
 	dma_addr_t dst_addr;
 	int chanidx;
@@ -37,7 +37,7 @@ struct odp_rx {
 	u32 sg_len;
 	dma_cookie_t cookie;
 	void *entry_addr;
-	u32 len; /* avoid to re-read the entry in the RX 2nd step */
+	u32 len;		/* avoid to re-read the entry in the RX 2nd step */
 };
 
 struct odp_cache_entry {
@@ -50,7 +50,7 @@ struct odp_if_priv {
 	struct napi_struct napi;
 
 	struct mppa_pcie_device *pdata;
-	struct pci_dev *pdev; /* pointer to device structure */
+	struct pci_dev *pdev;	/* pointer to device structure */
 
 	struct dentry *dir;
 
@@ -63,8 +63,9 @@ struct odp_if_priv {
 	u8 __iomem *interrupt_status_addr;
 
 	/* TX ring */
-	struct dma_chan *tx_chan[ODP_NOC_CHAN_COUNT+1];
-	struct mppa_pcie_dma_slave_config tx_config[ODP_NOC_CHAN_COUNT+1];
+	struct dma_chan *tx_chan[ODP_NOC_CHAN_COUNT + 1];
+	struct mppa_pcie_dma_slave_config tx_config[ODP_NOC_CHAN_COUNT +
+						    1];
 	struct odp_tx *tx_ring;
 
 	/* Position of the latest complete Tx buffer.
@@ -84,7 +85,7 @@ struct odp_if_priv {
 	int tx_mppa_size;
 	/* Number of descriptors on the host */
 	int tx_size;
-	struct timer_list tx_timer; /* checks Tx queues */
+	struct timer_list tx_timer;	/* checks Tx queues */
 	struct mppa_pcie_time *tx_time;
 	uint64_t packet_id;
 
@@ -108,19 +109,19 @@ struct odp_if_priv {
 	u8 __iomem *rx_head_addr;
 	int rx_size;
 	struct odp_c2h_ring_buff_entry *rx_mppa_entries;
-	struct timer_list rx_timer; /* checks Tx queues */
+	struct timer_list rx_timer;	/* checks Tx queues */
 
 };
 
 struct odp_pdata_priv {
-	struct list_head link; /* List of all devices */
+	struct list_head link;	/* List of all devices */
 	struct mppa_pcie_device *pdata;
-	struct pci_dev *pdev; /* pointer to device structure */
+	struct pci_dev *pdev;	/* pointer to device structure */
 	atomic_t state;
 	struct net_device *dev[ODP_MAX_IF_COUNT];
 	int if_count;
 	struct odp_control control;
-	struct work_struct enable; /* cannot register in interrupt context */
+	struct work_struct enable;	/* cannot register in interrupt context */
 	struct notifier_block notifier;
 };
 
