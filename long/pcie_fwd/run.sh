d859a6f1d5f55cb7ad98c4208511d06de4f21f47
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-05-31 17:03:05 +0200

    long: pcie_fwd slightly increase timeout
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- long/pcie_fwd/run.sh
+++ long/pcie_fwd/run.sh
@@ -14,8 +14,8 @@ echo 1 > /mppa/board0/mppa0/reset
 k1-jtag-runner --multibinary=pcie_fwd_multibin.mpk --exec-multibin=IODDR0:iopcie --exec-multibin=IODDR1:iopcie -- -cpcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 10" &
 sleep 5
 
-#10s for 10 pings (allow 1 drop)
-ping6 -c 10 -w 10 -I modp0.0.0.0 fe80::de:adff:febe:ef80
+#11s for 10 pings (allow 1 drop)
+ping6 -c 10 -w 11 -I modp0.0.0.0 fe80::de:adff:febe:ef80
 res=$?
 
 # Kill jtag and join it
