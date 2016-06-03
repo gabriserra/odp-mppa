1b263d92fc1f4fe2908ea6c8c78fbdff43be29ff
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-06-03 11:07:22 +0200

    apps: l2fwd-pcie: add l2fwd for PCI tests
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

---
+++ apps/l2fwd-pcie/run.sh
@@ -0,0 +1,6 @@
+CUR_DIR=$(readlink -e $(dirname $0))
+cd $CUR_DIR
+
+FIRMWARE=firmware.kelf
+
+exec k1-jtag-runner --progress --multibinary=l2fwd-pcie.mpk --exec-multibin=IODDR0:${FIRMWARE} --exec-multibin=IODDR1:${FIRMWARE} -- "$@"
