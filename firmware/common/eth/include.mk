803502ae57c227b79d86f7974400f2a04103970f
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-09-15 14:23:09 +0200

    firmare: eth: li2c requires lsysctl
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/eth/include.mk
+++ firmware/common/eth/include.mk
@@ -3,7 +3,7 @@ __ETH_INCLUDED__ := 1
 
 SRCDIRS  += $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 _CFLAGS  += -I$(TOP_SRCDIR)/include
-_LDFLAGS += -lmppapower -lmppanoc -lmpparouting -lmppaeth_utils -lmppaeth_88E1111 -li2c -lphy \
+_LDFLAGS += -lmppapower -lmppanoc -lmpparouting -lmppaeth_utils -lmppaeth_88E1111 -li2c -lsysctl -lphy \
 	 -Wl,--undefined=__eth_rpc_constructor
 
 endif
