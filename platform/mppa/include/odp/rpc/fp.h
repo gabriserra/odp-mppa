1e51a56c5e99b0d71683c924a4a31973aa19b27b
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-10-26 14:00:54 +0200

    mppa: rpc/fp: add 6Wind FP RPC service
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

---
+++ platform/mppa/include/odp/rpc/fp.h
@@ -0,0 +1,19 @@
+#ifndef __ODP_RPC_FP_H__
+#define __ODP_RPC_FP_H__
+
+#include <odp/rpc/defines.h>
+
+/** Version of the FP CoS */
+#define MPPA_RPC_ODP_FP_VERSION 0x2 /* Entirely arbitrary */
+
+typedef enum {
+	MPPA_RPC_ODP_CMD_FP_CLI,
+	MPPA_RPC_ODP_CMD_FP_N_CMD
+} mppa_rpc_odp_cmd_fp_e;
+
+#define MPPA_RPC_ODP_CMD_NAMES_FP \
+	"CLI",
+
+#define MPPA_RPC_ODP_ACK_LIST_FP
+
+#endif /* __ODP_RPC_FP_H__ */
