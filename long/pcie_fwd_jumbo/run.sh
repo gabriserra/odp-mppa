6f9f0e66d999ca979170cec3eb426a1adf0eda9b
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-10-27 13:35:57 +0200

    long: pcie_fwd_jumbo: full patch to ifconfig
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- long/pcie_fwd_jumbo/run.sh
+++ long/pcie_fwd_jumbo/run.sh
@@ -8,15 +8,15 @@ k1-jtag-runner --multibinary=pcie_fwd_jumbo_multibin.mpk --exec-multibin=IODDR0:
 			   --exec-multibin=IODDR1:firmware --chip-freq=400 -- \
 			   -c 0  &
 sleep 4
-sudo ifconfig modp0.0.0.0 mtu 9000
+sudo /usr/sbin/ifconfig modp0.0.0.0 mtu 9000
 if [ $? -eq 0 ]; then
     kill %
     wait
     exit 1
 fi
 
-sudo ifconfig modp0.0.0.0 mtu 8000
-sudo ifconfig modp0.0.1.0 mtu 8000
+sudo /usr/sbin/ifconfig modp0.0.0.0 mtu 8000
+sudo /usr/sbin/ifconfig modp0.0.1.0 mtu 8000
 #11s for 10 pings (allow 1 drop)
 ping6 -c 10 -w 11 -s 7000 -I modp0.0.0.0 fe80::de:adff:febe:ef80
 res=$?
