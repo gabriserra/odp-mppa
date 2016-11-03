225b0c4cecf4a47deda3b02a6b65dc40a03f1d5e
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-11-03 19:52:55 +0100

    build.rb: fix targets for debian 8
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- valid/hudson/build.rb
+++ valid/hudson/build.rb
@@ -103,11 +103,7 @@ if ENV["label"].to_s() != "" then
         configs= [ "k1b-kalray-nodeos_explorer", "k1b-kalray-mos_explorer" ]
         # Validate nothing.
         valid_configs = [ ]
-    when "debian7-64","debian8-64"
-	    configs = [ "k1b-kalray-mos_developer", "k1b-kalray-mos_konic80" ]
-        # Validate nothing.
-        valid_configs = [ ]
-    when "centos7-64"
+    when "centos7-64", "debian7-64","debian8-64"
         valid_configs = "k1b-kalray-nodeos_simu", "k1b-kalray-mos_simu"
         valid_type = "sim"
     when /MPPAExplorers_k1b*/
