gabriserra/odp-mppa

commit 1715d736b7ed7fd6ea89b38c665cac21ed724f43

firmware: eth: added basic link status/stats counter query
Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

firmware/common/eth/eth.c
@@ -188,7 +188,35 @@ odp_rpc_ack_t  eth_dual_mac(unsigned remoteClus __attribute__((unused)),
 	return ack;
 }
 
+odp_rpc_ack_t eth_get_stat(unsigned remoteClus __attribute__((unused)),
+			   odp_rpc_t *msg, uint8_t *ack_payload,
+			   uint16_t *ack_payload_len)
+{
+	odp_rpc_ack_t ack = { .status = 0 };
+	odp_rpc_cmd_eth_get_stat_t data = { .inl_data = msg->inl_data };
 
+	*ack_payload_len = 0;
+	if (data.ifId != 4 && data.ifId > N_ETH_LANE) {
+		ack.status = -1;
+		return ack;
+	}
+	if (data.ifId == 4 && status[0].initialized != ETH_LANE_ON_40G) {
+		ack.status = -1;
+		return ack;
+	} else if (data.ifId < N_ETH_LANE &&
+		   status[data.ifId].initialized !=! ETH_LANE_ON) {
+		ack.status = -1;
+		return ack;
+	}
+	ack.cmd.eth_get_stat.link_status = ethtool_poll_lane(data.ifId);
+
+	if (data.link_stats) {
+		*ack_payload_len = sizeof(odp_rpc_payload_eth_get_stat_t);
+		ethtool_lane_stats(data.ifId,
+				   (odp_rpc_payload_eth_get_stat_t *)ack_payload);
+	}
+	return ack;
+}
 static void eth_init(void)
 {
 	_eth_lb_status_init(&lb_status);
@@ -209,6 +237,8 @@ static void eth_init(void)
 static int eth_rpc_handler(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload)
 {
 	odp_rpc_ack_t ack = ODP_RPC_CMD_ACK_INITIALIZER;
+	uint16_t ack_payload_len = 0;
+	uint8_t ack_payload[RPC_MAX_PAYLOAD] __attribute__((aligned(8)));
 
 	switch (msg->pkt_type){
 	case ODP_RPC_CMD_ETH_OPEN:
@@ -230,7 +260,7 @@ static int eth_rpc_handler(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload
 	default:
 		return -1;
 	}
-	odp_rpc_server_ack(msg, ack, NULL, 0);
+	odp_rpc_server_ack(msg, ack, ack_payload, ack_payload_len);
 	return 0;
 }
 

firmware/common/eth/ethtools.c
@@ -787,3 +787,67 @@ int ethtool_set_dual_mac(int enable)
 	lb_status.dual_mac = enable;
 	return 0;
 }
+
+int ethtool_poll_lane(unsigned if_id)
+{
+	const int eth_if = if_id % 4;
+	enum mppa_eth_mac_ethernet_mode_e link_speed =
+		ethtool_get_mac_speed(if_id);
+	if (!mppa_eth_utils_mac_poll_state(eth_if, link_speed)) {
+		/* Link is up */
+		return 1;
+	}
+	/* Link is down */
+	return 0;
+}
+int ethtool_lane_stats(unsigned if_id,
+		       odp_rpc_payload_eth_get_stat_t *stats)
+{
+	const int eth_if = if_id % 4;
+
+	stats->in_octets =
+		mppabeth_mac_get_good_rx_bytes_nb((void *)&(mppa_ethernet[0]->mac),
+						   eth_if);
+	stats->in_ucast_pkts =
+		mppabeth_mac_get_good_rx_packet_nb((void *)&(mppa_ethernet[0]->mac),
+						   eth_if)-
+		mppabeth_mac_get_rx_multicast((void *)&(mppa_ethernet[0]->mac),
+					      eth_if) -
+		mppabeth_mac_get_rx_broadcast((void *)&(mppa_ethernet[0]->mac),
+					      eth_if);
+	stats->in_discards =
+		mppabeth_lb_get_dropped_counter((void*)&(mppa_ethernet[0]->lb),
+						eth_if);;
+	stats->in_errors =
+ 		mppabeth_mac_get_total_rx_packet_nb((void *)&(mppa_ethernet[0]->mac),
+						    eth_if) -
+ 		mppabeth_mac_get_good_rx_packet_nb((void *)&(mppa_ethernet[0]->mac),
+						   eth_if);
+
+	stats->in_unknown_protos = 0;
+	stats->out_octets =
+ 		mppabeth_mac_get_total_tx_bytes_nb((void *)&(mppa_ethernet[0]->mac),
+						   eth_if);
+
+	stats->out_ucast_pkts =
+		mppabeth_mac_get_total_tx_packet_nb((void *)&(mppa_ethernet[0]->mac),
+						   eth_if)-
+		mppabeth_mac_get_tx_multicast((void *)&(mppa_ethernet[0]->mac),
+					      eth_if) -
+		mppabeth_mac_get_tx_broadcast((void *)&(mppa_ethernet[0]->mac),
+					      eth_if);
+
+	stats->out_discards = 0;
+	for (int i = 0; i < MPPA_ETHERNET_TX_FIFO_IF_NUMBER; ++i){
+		for (int j = 0; j < MPPA_ETHERNET_LANE_ETH_FIFO_NUMBER; ++j){
+			stats->out_discards +=
+				mppa_ethernet[0]->tx.fifo_if[i].lane[eth_if].eth_fifo[j].dropped_pkt_cnt.reg;
+		}
+	}
+	stats->out_errors =
+		mppa_ethernet[0]->mac.lane_stat[0].tx_bad_fcs.reg +
+		mppa_ethernet[0]->mac.lane_stat[0].tx_bad_size.reg;
+
+	return 0;
+}
+

firmware/common/eth/internal/eth.h
@@ -39,6 +39,10 @@ int ethtool_disable_cluster(unsigned remoteClus, unsigned if_id);
 int ethtool_start_lane(unsigned if_id, int loopback);
 int ethtool_stop_lane(unsigned if_id);
 
+int ethtool_poll_lane(unsigned if_id);
+int ethtool_lane_stats(unsigned if_id,
+		       odp_rpc_payload_eth_get_stat_t *stats);
+
 int ethtool_set_dual_mac(int enabled);
 
 typedef enum {

platform/mppa/include/odp/rpc/eth.h
@@ -13,7 +13,8 @@
 		ODP_RPC_CMD_ETH_OPEN_DEF /**< ETH: Forward unmatch Rx traffic to a cluster */,		\
 		ODP_RPC_CMD_ETH_CLOS_DEF /**< ETH: Stop forwarding unmatch Rx traffic to a cluster */,	\
 		ODP_RPC_CMD_ETH_DUAL_MAC /**< ETH: Enable dual-mac mode (ODP + Linux) */,		\
-		ODP_RPC_CMD_ETH_STATE    /**< ETH: Start or Stop trafic forwarding */
+		ODP_RPC_CMD_ETH_STATE    /**< ETH: Start or Stop trafic forwarding */,			\
+		ODP_RPC_CMD_ETH_GET_STAT /**< ETH: GEt link status and statistics */			\
 
 #define ODP_RPC_CMD_NAMES_ETH			\
 	"ETH OPEN",				\
@@ -22,9 +23,13 @@
 		"ETH OPEN FALLTHROUGH",		\
 		"ETH CLOSE FALLTHROUGH",	\
 		"ETH DUAL MAC",			\
-		"ETH SET STATE"
+		"ETH SET STATE",		\
+		"ETH GET STATS"
 
-#define ODP_RPC_ACK_LIST_ETH odp_rpc_ack_eth_open_t eth_open;
+#define ODP_RPC_ACK_LIST_ETH				\
+	odp_rpc_ack_eth_open_t eth_open;		\
+	odp_rpc_ack_eth_get_stat_t eth_get_stat;
+
 /**
  * Command for ODP_RPC_CMD_ETH_OPEN
  */
@@ -106,6 +111,112 @@ typedef union {
 	odp_rpc_inl_data_t inl_data;
 } odp_rpc_cmd_eth_dual_mac_t;
 ODP_RPC_CHECK_STRUCT_SIZE(odp_rpc_cmd_eth_dual_mac_t);
+
+/**
+ * Command for ODP_RPC_CMD_ETH_GET_STAT
+ */
+typedef union {
+	struct {
+		uint8_t ifId : 3; /* 0-3, 4 for 40G */
+		uint8_t link_stats : 1; /* If enable returns link stats
+					 * counters in payload. If disabled
+					 * just return link status */
+	};
+	odp_rpc_inl_data_t inl_data;
+} odp_rpc_cmd_eth_get_stat_t;
+ODP_RPC_CHECK_STRUCT_SIZE(odp_rpc_cmd_eth_get_stat_t);
+
+/**
+ * Ack inline for ODP_RPC_CMD_GET_STAT
+ */
+typedef struct {
+	uint16_t link_status;
+} odp_rpc_ack_eth_get_stat_t;
+
+/**
+ * Payload for ack of ODP_RPC_CMD_GET_STAT.
+ *
+ * Follows RFCs for Management Information Base
+ * (MIB)for use with network management protocols in the Internet community:
+ * https://tools.ietf.org/html/rfc3635
+ * https://tools.ietf.org/html/rfc2863
+ * https://tools.ietf.org/html/rfc2819
+ */
+typedef struct {
+	/**
+	 * The number of octets in valid MAC frames received on this interface,
+	 * including the MAC header and FCS. See ifHCInOctets counter
+	 * description in RFC 3635 for details.
+	 */
+	uint64_t in_octets;
+
+	/**
+	 * The number of packets, delivered by this sub-layer to a higher
+	 * (sub-)layer, which were not addressed to a multicast or broadcast
+	 * address at this sub-layer. See ifHCInUcastPkts in RFC 2863, RFC 3635.
+	 */
+	uint64_t in_ucast_pkts;
+
+	/**
+	 * The number of inbound packets which were chosen to be discarded
+	 * even though no errors had been detected to preven their being
+	 * deliverable to a higher-layer protocol.  One possible reason for
+	 * discarding such a packet could be to free up buffer space.
+	 * See ifInDiscards in RFC 2863.
+	 */
+	uint64_t in_discards;
+
+	/**
+	 * The sum for this interface of AlignmentErrors, FCSErrors, FrameTooLongs,
+	 * InternalMacReceiveErrors. See ifInErrors in RFC 3635.
+	 */
+	uint64_t in_errors;
+
+	/**
+	 * For packet-oriented interfaces, the number of packets received via
+	 * the interface which were discarded because of an unknown or
+	 * unsupported protocol.  For character-oriented or fixed-length
+	 * interfaces that support protocol multiplexing the number of
+	 * transmission units received via the interface which were discarded
+	 * because of an unknown or unsupported protocol.  For any interface
+	 * that does not support protocol multiplexing, this counter will always
+	 * be 0. See ifInUnknownProtos in RFC 2863, RFC 3635.
+	 */
+	uint64_t in_unknown_protos;
+
+	/**
+	 * The number of octets transmitted in valid MAC frames on this
+	 * interface, including the MAC header and FCS.  This does include
+	 * the number of octets in valid MAC Control frames transmitted on
+	 * this interface. See ifHCOutOctets in RFC 3635.
+	 */
+	uint64_t out_octets;
+
+	/**
+	 * The total number of packets that higher-level protocols requested
+	 * be transmitted, and which were not addressed to a multicast or
+	 * broadcast address at this sub-layer, including those that were
+	 * discarded or not sent. does not include MAC Control frames.
+	 * See ifHCOutUcastPkts RFC 2863, 3635.
+	 */
+	uint64_t out_ucast_pkts;
+
+	/**
+	 * The number of outbound packets which were chosen to be discarded
+	 * even though no errors had been detected to prevent their being
+	 * transmitted.  One possible reason for discarding such a packet could
+	 * be to free up buffer space.  See  OutDiscards in  RFC 2863.
+	 */
+	uint64_t out_discards;
+
+	/**
+	 * The sum for this interface of SQETestErrors, LateCollisions,
+	 * ExcessiveCollisions, InternalMacTransmitErrors and
+	 * CarrierSenseErrors. See ifOutErrors in RFC 3635.
+	 */
+	uint64_t out_errors;
+}  odp_rpc_payload_eth_get_stat_t;
+
 
 
 /*

commit 5aedd351d14f587971fb67526530665c25a6a981

firmware: fix stats retrieval for ports in 10G mode
Fixes: 1715d736b7ed ("firmware: eth: added basic link status/stats ...")

Signed-off-by: David Marchand <david.marchand@6wind.com>

firmware/common/eth/eth.c
@@ -213,7 +213,7 @@ void eth_get_stat(unsigned remoteClus __attribute__((unused)),
 		ETH_RPC_ERR_MSG(answer, "Trying to get stats on 40G lane while lane is closed or in a different mode\n");
 		return;
 	} else if (data.ifId < N_ETH_LANE &&
-		   status[data.ifId].initialized !=! ETH_LANE_ON) {
+		   status[data.ifId].initialized != ETH_LANE_ON) {
 		   ETH_RPC_ERR_MSG(answer, "Trying to get stats for 1/10G lane while lane is closed or in 40G\n");
 		   return;
 	}