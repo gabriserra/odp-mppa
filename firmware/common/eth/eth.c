gabriserra/odp-mppa

commit 64c38faf65bae7355e0dd3e756757150045eea29

firmware/mppa: cleanup all RPC error handling
RPC ack contains a bit to declare if payload contains an error string.
When firmware has an error, it does not print it anymore but send the
error string back through a RPC payload with the ack.

At the same time:
 * Use a struct to handle ack + payload in all firmwares
 * Cleanup many error paths with no error logs
 * Print error messages in ODP
 * Handle errors in crypto

Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

firmware/common/eth/eth.c
@@ -23,20 +23,20 @@ static inline int get_eth_dma_id(unsigned cluster_id){
 	return offset + ETH_BASE_TX;
 }
 
-odp_rpc_ack_t  eth_open(unsigned remoteClus, odp_rpc_t *msg,
-			    uint8_t *payload, unsigned fallthrough)
+void eth_open(unsigned remoteClus, odp_rpc_t *msg,
+	      uint8_t *payload, unsigned fallthrough,
+	      odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0};
 	odp_rpc_cmd_eth_open_t data = { .inl_data = msg->inl_data };
 	const int nocIf = get_eth_dma_id(data.dma_if);
 	const unsigned int eth_if = data.ifId % 4; /* 4 is actually 0 in 40G mode */
 
 	if(nocIf < 0) {
-		fprintf(stderr, "[ETH] Error: Invalid NoC interface (%d %d)\n", nocIf, remoteClus);
+		ETH_RPC_ERR_MSG(answer, "Invalid NoC interface (%d %d)\n", nocIf, remoteClus);
 		goto err;
 	}
-	if(data.ifId > 4 || eth_if >= N_ETH_LANE) {
-		fprintf(stderr, "[ETH] Error: Invalid Eth lane\n");
+	if (data.ifId != 4 && data.ifId > N_ETH_LANE) {
+		ETH_RPC_ERR_MSG(answer, "Bad lane id %d\n", data.ifId);
 		goto err;
 	}
 
@@ -44,14 +44,14 @@ odp_rpc_ack_t  eth_open(unsigned remoteClus, odp_rpc_t *msg,
 		/* 40G port. We need to check all lanes */
 		for (int i = 0; i < N_ETH_LANE; ++i) {
 			if(status[i].cluster[remoteClus].opened != ETH_CLUS_STATUS_OFF) {
-				fprintf(stderr, "[ETH] Error: Lane %d is already opened for cluster %d\n",
+				ETH_RPC_ERR_MSG(answer, "Lane %d is already opened for cluster %d\n",
 						i, remoteClus);
 				goto err;
 			}
 		}
 	} else {
 		if (status[eth_if].cluster[remoteClus].opened != ETH_CLUS_STATUS_OFF) {
-			fprintf(stderr, "[ETH] Error: Lane %d is already opened for cluster %d\n",
+			ETH_RPC_ERR_MSG(answer, "Lane %d is already opened for cluster %d\n",
 					eth_if, remoteClus);
 			goto err;
 		}
@@ -65,7 +65,7 @@ odp_rpc_ack_t  eth_open(unsigned remoteClus, odp_rpc_t *msg,
 	}
 
 	if (fallthrough && !lb_status.dual_mac) {
-		fprintf(stderr, "[ETH] Error: Trying to open in fallthrough with Dual-MAC mode disabled\n");
+		ETH_RPC_ERR_MSG(answer, "Trying to open in fallthrough with Dual-MAC mode disabled\n");
 		goto err;
 	}
 
@@ -75,97 +75,108 @@ odp_rpc_ack_t  eth_open(unsigned remoteClus, odp_rpc_t *msg,
 	status[eth_if].cluster[remoteClus].tx_enabled = data.tx_enabled;
 	status[eth_if].cluster[remoteClus].jumbo = data.jumbo;
 
-	if (ethtool_open_cluster(remoteClus, data.ifId))
+	if (ethtool_open_cluster(remoteClus, data.ifId, answer))
 		goto err;
 	if (ethtool_setup_eth2clus(remoteClus, data.ifId, nocIf, externalAddress,
-				   data.min_rx, data.max_rx))
+				   data.min_rx, data.max_rx, answer))
 		goto err;
-	if (ethtool_setup_clus2eth(remoteClus, data.ifId, nocIf))
+	if (ethtool_setup_clus2eth(remoteClus, data.ifId, nocIf, answer))
 		goto err;
 	if (fallthrough) {
 		status[eth_if].cluster[remoteClus].policy = ETH_CLUS_POLICY_FALLTHROUGH;
 	} else {
 		if ( ethtool_apply_rules(remoteClus, data.ifId,
-					 data.nb_rules, (pkt_rule_t*)payload))
+					 data.nb_rules,
+					 (pkt_rule_t*)payload, answer))
 			goto err;
 	}
-	if (ethtool_start_lane(data.ifId, data.loopback, data.verbose))
+	if (ethtool_start_lane(data.ifId, data.loopback, data.verbose, answer))
 		goto err;
 
-	ack.cmd.eth_open.tx_if = externalAddress;
-	ack.cmd.eth_open.tx_tag = status[eth_if].cluster[remoteClus].rx_tag;
+	answer->ack.cmd.eth_open.tx_if = externalAddress;
+	answer->ack.cmd.eth_open.tx_tag = status[eth_if].cluster[remoteClus].rx_tag;
 	if (data.jumbo) {
-		ack.cmd.eth_open.mtu = 9000;
+		answer->ack.cmd.eth_open.mtu = 9000;
 	} else {
-		ack.cmd.eth_open.mtu = 1600;
+		answer->ack.cmd.eth_open.mtu = 1600;
 	}
 
 	if (!lb_status.dual_mac || fallthrough) {
-		memcpy(ack.cmd.eth_open.mac, status[eth_if].mac_address[0], ETH_ALEN);
+		memcpy(answer->ack.cmd.eth_open.mac, status[eth_if].mac_address[0], ETH_ALEN);
 	} else {
-		memcpy(ack.cmd.eth_open.mac, status[eth_if].mac_address[1], ETH_ALEN);
+		memcpy(answer->ack.cmd.eth_open.mac, status[eth_if].mac_address[1], ETH_ALEN);
 	}
 
-	return ack;
+	return;
  err:
-	ethtool_close_cluster(remoteClus, data.ifId);
-	ack.status = 1;
-	return ack;
+	ethtool_close_cluster(remoteClus, data.ifId, NULL);
+	return;
 }
 
-odp_rpc_ack_t  eth_set_state(unsigned remoteClus, odp_rpc_t *msg)
+void eth_set_state(unsigned remoteClus, odp_rpc_t *msg,
+		   odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0 };
 	odp_rpc_cmd_eth_state_t data = { .inl_data = msg->inl_data };
 	const unsigned int eth_if = data.ifId % 4; /* 4 is actually 0 in 40G mode */
 
+	if (data.ifId != 4 && data.ifId > N_ETH_LANE) {
+		ETH_RPC_ERR_MSG(answer, "Bad lane id %d\n", data.ifId);
+		return;
+	}
+
 	if (data.ifId == 4) {
 		if(status[eth_if].cluster[remoteClus].opened != ETH_CLUS_STATUS_40G) {
-			ack.status = -1;
-			return ack;
+			ETH_RPC_ERR_MSG(answer, "Tring to set state for 40G lane while lane is closed or in a different mode\n");
+			return;
 		}
 	} else {
 		if(status[eth_if].cluster[remoteClus].opened != ETH_CLUS_STATUS_ON) {
-			ack.status = -1;
-			return ack;
+			ETH_RPC_ERR_MSG(answer, "Tring to set state for 1/10G lane while lane is closed or in 40G\n");
+			return;
 		}
 	}
 
 	if (data.enabled) {
-		if (ethtool_enable_cluster(remoteClus, data.ifId)) {
-			ack.status = -1;
-			return ack;
+		if (ethtool_enable_cluster(remoteClus, data.ifId, answer)) {
+			return;
 		}
 	} else {
-		if (ethtool_disable_cluster(remoteClus, data.ifId)) {
-			ack.status = -1;
-			return ack;
+		if (ethtool_disable_cluster(remoteClus, data.ifId, answer)) {
+			return;
 		}
 	}
 
-	return ack;
+	return;
 }
 
-odp_rpc_ack_t  eth_close(unsigned remoteClus, odp_rpc_t *msg)
+void eth_close(unsigned remoteClus, odp_rpc_t *msg,
+	       odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0 };
 	odp_rpc_cmd_eth_clos_t data = { .inl_data = msg->inl_data };
 	const unsigned int eth_if = data.ifId % 4; /* 4 is actually 0 in 40G mode */
 
+	if (data.ifId != 4 && data.ifId > N_ETH_LANE) {
+		ETH_RPC_ERR_MSG(answer, "Bad lane id %d\n", data.ifId);
+		return;
+	}
+
 	if (data.ifId == 4) {
 		if(status[eth_if].cluster[remoteClus].opened != ETH_CLUS_STATUS_40G) {
-			ack.status = -1;
-			return ack;
+			ETH_RPC_ERR_MSG(answer, "Tring to close 40G lane while lane is closed or in a different mode\n");
+			return;
 		}
 	} else {
 		if(status[eth_if].cluster[remoteClus].opened != ETH_CLUS_STATUS_ON) {
-			ack.status = -1;
-			return ack;
+			ETH_RPC_ERR_MSG(answer, "Tring to set state for 1/10G lane while lane is closed or in 40G\n");
+			return;
 		}
 	}
 
-	ethtool_disable_cluster(remoteClus, data.ifId);
-	ethtool_close_cluster(remoteClus, data.ifId);
+	if (status[eth_if].cluster[remoteClus].enabled)
+		if (ethtool_disable_cluster(remoteClus, data.ifId, answer))
+			return;
+	if (ethtool_close_cluster(remoteClus, data.ifId, answer))
+		return;
 
 	if (data.ifId == 4) {
 		for (int i = 0; i < N_ETH_LANE; ++i) {
@@ -175,47 +186,42 @@ odp_rpc_ack_t  eth_close(unsigned remoteClus, odp_rpc_t *msg)
 		_eth_cluster_status_init(&status[eth_if].cluster[remoteClus]);
 	}
 
-	return ack;
+	return;
 }
 
-odp_rpc_ack_t  eth_dual_mac(unsigned remoteClus __attribute__((unused)),
-				odp_rpc_t *msg)
+void eth_dual_mac(unsigned remoteClus __attribute__((unused)),
+		  odp_rpc_t *msg,
+		  odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0 };
 	odp_rpc_cmd_eth_dual_mac_t data = { .inl_data = msg->inl_data };
-	if (ethtool_set_dual_mac(data.enabled))
-		ack.status = -1;
-	return ack;
+	ethtool_set_dual_mac(data.enabled, answer);
+	return;
 }
 
-odp_rpc_ack_t eth_get_stat(unsigned remoteClus __attribute__((unused)),
-			   odp_rpc_t *msg, uint8_t *ack_payload,
-			   uint16_t *ack_payload_len)
+void eth_get_stat(unsigned remoteClus __attribute__((unused)),
+			   odp_rpc_t *msg,
+			   odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0 };
 	odp_rpc_cmd_eth_get_stat_t data = { .inl_data = msg->inl_data };
 
-	*ack_payload_len = 0;
 	if (data.ifId != 4 && data.ifId > N_ETH_LANE) {
-		ack.status = -1;
-		return ack;
+		ETH_RPC_ERR_MSG(answer, "Bad lane id %d\n", data.ifId);
+		return;
 	}
 	if (data.ifId == 4 && status[0].initialized != ETH_LANE_ON_40G) {
-		ack.status = -1;
-		return ack;
+		ETH_RPC_ERR_MSG(answer, "Tring to get stats on 40G lane while lane is closed or in a different mode\n");
+		return;
 	} else if (data.ifId < N_ETH_LANE &&
 		   status[data.ifId].initialized !=! ETH_LANE_ON) {
-		ack.status = -1;
-		return ack;
+		   ETH_RPC_ERR_MSG(answer, "Tring to set state for 1/10G lane while lane is closed or in 40G\n");
+		   return;
 	}
-	ack.cmd.eth_get_stat.link_status = ethtool_poll_lane(data.ifId);
+	answer->ack.cmd.eth_get_stat.link_status = ethtool_poll_lane(data.ifId);
 
 	if (data.link_stats) {
-		*ack_payload_len = sizeof(odp_rpc_payload_eth_get_stat_t);
-		ethtool_lane_stats(data.ifId,
-				   (odp_rpc_payload_eth_get_stat_t *)ack_payload);
+		ethtool_lane_stats(data.ifId, answer);
 	}
-	return ack;
+	return;
 }
 static void eth_init(void)
 {
@@ -236,9 +242,7 @@ static void eth_init(void)
 
 static int eth_rpc_handler(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload)
 {
-	odp_rpc_ack_t ack = ODP_RPC_CMD_ACK_INITIALIZER;
-	uint16_t ack_payload_len = 0;
-	uint8_t ack_payload[RPC_MAX_PAYLOAD] __attribute__((aligned(8)));
+	odp_rpc_answer_t answer = ODP_RPC_ANSWER_INITIALIZER(msg);
 
 	if (msg->pkt_class != ODP_RPC_CLASS_ETH)
 		return -ODP_RPC_ERR_INTERNAL_ERROR;
@@ -247,29 +251,29 @@ static int eth_rpc_handler(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload
 
 	switch (msg->pkt_subtype){
 	case ODP_RPC_CMD_ETH_OPEN:
-		ack = eth_open(remoteClus, msg, payload, 0);
+		eth_open(remoteClus, msg, payload, 0, &answer);
 		break;
 	case ODP_RPC_CMD_ETH_STATE:
-		ack = eth_set_state(remoteClus, msg);
+		eth_set_state(remoteClus, msg, &answer);
 		break;
 	case ODP_RPC_CMD_ETH_CLOS:
 	case ODP_RPC_CMD_ETH_CLOS_DEF:
-		ack = eth_close(remoteClus, msg);
+		eth_close(remoteClus, msg, &answer);
 		break;
 	case ODP_RPC_CMD_ETH_OPEN_DEF:
-		ack = eth_open(remoteClus, msg, payload, 1);
+		eth_open(remoteClus, msg, payload, 1, &answer);
 		break;
 	case ODP_RPC_CMD_ETH_DUAL_MAC:
-		ack = eth_dual_mac(remoteClus, msg);
+		eth_dual_mac(remoteClus, msg, &answer);
 		break;
 	case ODP_RPC_CMD_ETH_GET_STAT:
-		ack = eth_get_stat(remoteClus, msg, ack_payload, &ack_payload_len);
+		eth_get_stat(remoteClus, msg, &answer);
 		break;
 	default:
 		return -ODP_RPC_ERR_BAD_SUBTYPE;
 	}
 
-	odp_rpc_server_ack(msg, ack, ack_payload, ack_payload_len);
+	odp_rpc_server_ack(&answer);
 	return -ODP_RPC_ERR_NONE;
 }
 

platform/mppa/pktio/eth.c
@@ -71,6 +71,7 @@ static int eth_rpc_send_eth_open(odp_pktio_param_t * params, pkt_eth_t *eth, int
 	odp_rpc_t *ack_msg;
 	odp_rpc_ack_t ack;
 	int ret;
+	uint8_t *payload;
 
 	/*
 	 * RPC Msg to IOETH  #N so the LB will dispatch to us
@@ -108,7 +109,7 @@ static int eth_rpc_send_eth_open(odp_pktio_param_t * params, pkt_eth_t *eth, int
 					 odp_rpc_get_io_tag_id(cluster_id),
 					 &cmd, rules);
 
-	ret = odp_rpc_wait_ack(&ack_msg, NULL, 15 * ODP_RPC_TIMEOUT_1S);
+	ret = odp_rpc_wait_ack(&ack_msg, (void**)&payload, 15 * ODP_RPC_TIMEOUT_1S);
 	if (ret < 0) {
 		fprintf(stderr, "[ETH] RPC Error\n");
 		return 1;
@@ -120,6 +121,8 @@ static int eth_rpc_send_eth_open(odp_pktio_param_t * params, pkt_eth_t *eth, int
 	ack.inl_data = ack_msg->inl_data;
 	if (ack.status) {
 		fprintf(stderr, "[ETH] Error: Server declined opening of eth interface\n");
+		if (ack_msg->err_str && ack_msg->data_len > 0)
+			fprintf(stderr, "[ETH] Error Log: %s\n", payload);
 		return 1;
 	}
 
@@ -514,6 +517,7 @@ static int eth_close(pktio_entry_t * const pktio_entry)
 		.cos_version = ODP_RPC_ETH_VERSION,
 		.inl_data = close_cmd.inl_data
 	};
+	uint8_t *payload;
 
 	/* Free packets being sent by DMA */
 	tx_uc_flush(eth_get_ctx(eth));
@@ -522,7 +526,7 @@ static int eth_close(pktio_entry_t * const pktio_entry)
 					 odp_rpc_get_io_tag_id(cluster_id),
 					 &cmd, NULL);
 
-	ret = odp_rpc_wait_ack(&ack_msg, NULL, 5 * ODP_RPC_TIMEOUT_1S);
+	ret = odp_rpc_wait_ack(&ack_msg, (void**)&payload, 5 * ODP_RPC_TIMEOUT_1S);
 	if (ret < 0) {
 		fprintf(stderr, "[ETH] RPC Error\n");
 		return 1;
@@ -531,6 +535,11 @@ static int eth_close(pktio_entry_t * const pktio_entry)
 		return 1;
 	}
 	ack.inl_data = ack_msg->inl_data;
+	if (ack.status) {
+		fprintf(stderr, "[ETH] Error: Server declined closure of eth interface\n");
+		if (ack_msg->err_str && ack_msg->data_len > 0)
+			fprintf(stderr, "[ETH] Error Log: %s\n", payload);
+	}
 
 	/* Push Context to handling threads */
 	rx_thread_link_close(eth->rx_config.pktio_id);
@@ -562,12 +571,13 @@ static int eth_set_state(pktio_entry_t * const pktio_entry, int enabled)
 		.cos_version = ODP_RPC_ETH_VERSION,
 		.inl_data = state_cmd.inl_data
 	};
+	uint8_t *payload;
 
 	odp_rpc_do_query(odp_rpc_get_io_dma_id(slot_id, cluster_id),
 					 odp_rpc_get_io_tag_id(cluster_id),
 					 &cmd, NULL);
 
-	ret = odp_rpc_wait_ack(&ack_msg, NULL, 5 * ODP_RPC_TIMEOUT_1S);
+	ret = odp_rpc_wait_ack(&ack_msg, (void**)&payload, 5 * ODP_RPC_TIMEOUT_1S);
 	if (ret < 0) {
 		fprintf(stderr, "[ETH] RPC Error\n");
 		return 1;
@@ -576,6 +586,13 @@ static int eth_set_state(pktio_entry_t * const pktio_entry, int enabled)
 		return 1;
 	}
 	ack.inl_data = ack_msg->inl_data;
+	if (ack.status) {
+		fprintf(stderr, "[ETH] Error: Server declined change of eth state\n");
+		if (ack_msg->err_str && ack_msg->data_len > 0)
+			fprintf(stderr, "[ETH] Error Log: %s\n", payload);
+		return 1;
+	}
+
 	return ack.status;
 }
 
@@ -681,7 +698,7 @@ static int eth_stats(pktio_entry_t *const pktio_entry,
 	odp_rpc_t *ack_msg;
 	odp_rpc_ack_t ack;
 	int ret;
-	void *payload;
+	uint8_t *payload;
 	odp_rpc_payload_eth_get_stat_t *rpc_stats;
 
 	/*
@@ -707,7 +724,7 @@ static int eth_stats(pktio_entry_t *const pktio_entry,
 					 odp_rpc_get_io_tag_id(cluster_id),
 					 &cmd, NULL);
 
-	ret = odp_rpc_wait_ack(&ack_msg, &payload, 2 * ODP_RPC_TIMEOUT_1S);
+	ret = odp_rpc_wait_ack(&ack_msg, (void**)&payload, 2 * ODP_RPC_TIMEOUT_1S);
 	if (ret < 0) {
 		return -1;
 	} else if (ret == 0){
@@ -716,6 +733,9 @@ static int eth_stats(pktio_entry_t *const pktio_entry,
 
 	ack.inl_data = ack_msg->inl_data;
 	if (ack.status) {
+		fprintf(stderr, "[ETH] Error: Server declined retrieval of eth stats\n");
+		if (ack_msg->err_str && ack_msg->data_len > 0)
+			fprintf(stderr, "[ETH] Error Log: %s\n", payload);
 		return -1;
 	}
 

firmware/common/c2c/c2c.c
@@ -10,6 +10,11 @@
 
 #include "internal/rpc-server.h"
 
+#define C2C_RPC_ERR_MSG(answer, x...)			\
+	do {						\
+		RPC_ERROR(answer, "[C2C]", ##x);	\
+	} while(0)
+
 typedef struct {
 	uint8_t opened     : 1;
 	uint8_t rx_enabled : 1;
@@ -22,16 +27,16 @@ typedef struct {
 
 static c2c_status_t c2c_status[RPC_MAX_CLIENTS][RPC_MAX_CLIENTS];
 
-odp_rpc_ack_t  c2c_open(unsigned src_cluster, odp_rpc_t *msg)
+void c2c_open(unsigned src_cluster, odp_rpc_t *msg,
+			odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0};
 	odp_rpc_cmd_c2c_open_t data = { .inl_data = msg->inl_data };
 	const unsigned dst_cluster = data.cluster_id;
 
 	if (c2c_status[src_cluster][dst_cluster].opened){
-		fprintf(stderr, "[C2C] Error: %d => %d is already opened\n",
-			src_cluster, dst_cluster);
-		goto err;
+		C2C_RPC_ERR_MSG(answer, "Cluster2Cluster link %d => %d is already opened\n",
+				src_cluster, dst_cluster);
+		return;
 	}
 
 	c2c_status[src_cluster][dst_cluster].opened = 1;
@@ -41,36 +46,28 @@ odp_rpc_ack_t  c2c_open(unsigned src_cluster, odp_rpc_t *msg)
 	c2c_status[src_cluster][dst_cluster].max_rx = data.max_rx;
 	c2c_status[src_cluster][dst_cluster].rx_size = data.mtu;
 	c2c_status[src_cluster][dst_cluster].cnoc_rx = data.cnoc_rx;
-	return ack;
-
- err:
-	ack.status = 1;
-	return ack;
+	return;
 }
 
-odp_rpc_ack_t  c2c_close(unsigned src_cluster, odp_rpc_t *msg)
+void c2c_close(unsigned src_cluster, odp_rpc_t *msg,
+	       odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0};
 	odp_rpc_cmd_c2c_clos_t data = { .inl_data = msg->inl_data };
 	const unsigned dst_cluster = data.cluster_id;
 
 	if (!c2c_status[src_cluster][dst_cluster].opened){
-		fprintf(stderr, "[C2C] Error: %d => %d is not open\n",
-			src_cluster, dst_cluster);
-		goto err;
+		C2C_RPC_ERR_MSG(answer, "Cluster2Cluster link %d => %d is not open\n",
+				src_cluster, dst_cluster);
+		return;
 	}
 
 	memset(&c2c_status[src_cluster][dst_cluster], 0, sizeof(c2c_status_t));
-	return ack;
-
- err:
-	ack.status = 1;
-	return ack;
+	return;
 }
 
-odp_rpc_ack_t  c2c_query(unsigned src_cluster, odp_rpc_t *msg)
+void c2c_query(unsigned src_cluster, odp_rpc_t *msg,
+	       odp_rpc_answer_t *answer)
 {
-	odp_rpc_ack_t ack = { .status = 0};
 	odp_rpc_cmd_c2c_query_t data = { .inl_data = msg->inl_data };
 	const unsigned dst_cluster = data.cluster_id;
 
@@ -78,29 +75,29 @@ odp_rpc_ack_t  c2c_query(unsigned src_cluster, odp_rpc_t *msg)
 	const c2c_status_t * d2s = &c2c_status[dst_cluster][src_cluster];
 
 	if (!s2d->opened || !d2s->opened){
-		ack.status = 1;
-		ack.cmd.c2c_query.closed = 1;
-		return ack;
+		answer->ack.status = 1;
+		answer->ack.cmd.c2c_query.closed = 1;
+		return;
 	}
 
 	if (!s2d->tx_enabled || !d2s->rx_enabled) {
-		ack.status = 1;
-		ack.cmd.c2c_query.eacces = 1;
-		return ack;
+		answer->ack.status = 1;
+		answer->ack.cmd.c2c_query.eacces = 1;
+		return;
 	}
-	ack.cmd.c2c_query.mtu = d2s->rx_size;
-	if (s2d->rx_size < ack.cmd.c2c_query.mtu)
-		ack.cmd.c2c_query.mtu = s2d->rx_size;
+	answer->ack.cmd.c2c_query.mtu = d2s->rx_size;
+	if (s2d->rx_size < answer->ack.cmd.c2c_query.mtu)
+		answer->ack.cmd.c2c_query.mtu = s2d->rx_size;
 
-	ack.cmd.c2c_query.min_rx = d2s->min_rx;
-	ack.cmd.c2c_query.max_rx = d2s->max_rx;
-	ack.cmd.c2c_query.cnoc_rx = d2s->cnoc_rx;
-	return ack;
+	answer->ack.cmd.c2c_query.min_rx = d2s->min_rx;
+	answer->ack.cmd.c2c_query.max_rx = d2s->max_rx;
+	answer->ack.cmd.c2c_query.cnoc_rx = d2s->cnoc_rx;
+	return;
 }
 
 static int c2c_rpc_handler(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload)
 {
-	odp_rpc_ack_t ack = ODP_RPC_CMD_ACK_INITIALIZER;
+	odp_rpc_answer_t answer = ODP_RPC_ANSWER_INITIALIZER(msg);
 
 	if (msg->pkt_class != ODP_RPC_CLASS_C2C)
 		return -ODP_RPC_ERR_INTERNAL_ERROR;
@@ -110,18 +107,18 @@ static int c2c_rpc_handler(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload
 	(void)payload;
 	switch (msg->pkt_subtype){
 	case ODP_RPC_CMD_C2C_OPEN:
-		ack = c2c_open(remoteClus, msg);
+		c2c_open(remoteClus, msg, &answer);
 		break;
 	case ODP_RPC_CMD_C2C_CLOS:
-		ack = c2c_close(remoteClus, msg);
+		c2c_close(remoteClus, msg, &answer);
 		break;
 	case ODP_RPC_CMD_C2C_QUERY:
-		ack = c2c_query(remoteClus, msg);
+		c2c_query(remoteClus, msg, &answer);
 		break;
 	default:
 		return -ODP_RPC_ERR_BAD_SUBTYPE;
 	}
-	odp_rpc_server_ack(msg, ack, NULL, 0);
+	odp_rpc_server_ack(&answer);
 	return -ODP_RPC_ERR_NONE;
 }
 

commit 3b7b7a3a0385890138b24d54db531e991ee9ced7

firmware: fix logs when retrieving eth stats
Fixes: 64c38faf65ba ("firmware/mppa: cleanup all RPC error handling")

Signed-off-by: David Marchand <david.marchand@6wind.com>

firmware/common/eth/eth.c
@@ -210,11 +210,11 @@ void eth_get_stat(unsigned remoteClus __attribute__((unused)),
 		return;
 	}
 	if (data.ifId == 4 && status[0].initialized != ETH_LANE_ON_40G) {
-		ETH_RPC_ERR_MSG(answer, "Tring to get stats on 40G lane while lane is closed or in a different mode\n");
+		ETH_RPC_ERR_MSG(answer, "Trying to get stats on 40G lane while lane is closed or in a different mode\n");
 		return;
 	} else if (data.ifId < N_ETH_LANE &&
 		   status[data.ifId].initialized !=! ETH_LANE_ON) {
-		   ETH_RPC_ERR_MSG(answer, "Tring to set state for 1/10G lane while lane is closed or in 40G\n");
+		   ETH_RPC_ERR_MSG(answer, "Trying to get stats for 1/10G lane while lane is closed or in 40G\n");
 		   return;
 	}
 	answer->ack.cmd.eth_get_stat.link_status = ethtool_poll_lane(data.ifId);