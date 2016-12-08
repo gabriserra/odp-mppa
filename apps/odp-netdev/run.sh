81a2da8b26b8f1cfaa127d687df30984497010b1
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-12-08 12:02:54 +0100

    apps: odp-netdev: add apps tu use KONIC as a netdev (One eth per I/O)
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

---
+++ apps/odp-netdev/run.sh
@@ -0,0 +1,6 @@
+CUR_DIR=$(readlink -e $(dirname $0))
+cd $CUR_DIR
+
+FIRMWARE=firmware.kelf
+
+exec k1-jtag-runner --progress --multibinary=odp-netdev.mpk --exec-multibin=IODDR0:${FIRMWARE} --exec-multibin=IODDR1:${FIRMWARE} -- "$@"
