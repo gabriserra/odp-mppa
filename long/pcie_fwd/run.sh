12b0fbbe052823e22048208bb4cf0d279294e95c
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2017-01-05 17:50:12 +0100

    long: pcie: intensify pcie tests
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- long/pcie_fwd/run.sh
+++ long/pcie_fwd/run.sh
@@ -6,22 +6,34 @@ cd $CUR_DIR
 #Start jtag runner in the BG
 res=1
 
-for i in $(seq 1 5); do
-    k1-jtag-runner --multibinary=pcie_fwd_multibin.mpk --exec-multibin=IODDR0:iopcie \
-		   --exec-multibin=IODDR1:iopcie --chip-freq=400 -- -cpcie_fwd \
-		   -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" &
-    sleep 5
+k1-jtag-runner --multibinary=pcie_fwd_multibin.mpk --exec-multibin=IODDR0:iopcie \
+	       --exec-multibin=IODDR1:iopcie --chip-freq=400 -- \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" \
+	       -c pcie_fwd -a "-i p0p0:tags=60,p1p0:tags=60" -a "-m 0" -a "-s 0" -a "-c 8" > /dev/null &
+sleep 15
 
-    #11s for 10 pings (allow 1 drop)
+#11s for 10 pings (allow 1 drop)
+(sudo ping6 -I modp0.0.0.0 -f fe80::de:adff:febe:ef80 -c 8192 -w 10 > /dev/null) && \
     ping6 -c 10 -w 11 -I modp0.0.0.0 fe80::de:adff:febe:ef80
-    res=$?
+res=$?
 
-    # Kill jtag and join it
-    kill %
-    wait
-
-    if [ $res -eq 0 ]; then break; fi
-done
+# Kill jtag and join it
+kill %
+wait
 
 
 exit $res
