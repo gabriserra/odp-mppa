af4dd7a3e69e6206f187bd80b60f60dc6a6f4f5a
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2017-01-19 10:33:31 +0100

    long: pcie_fwd_multiq: sleep between ping runs
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- long/pcie_fwd_multiq/run.sh
+++ long/pcie_fwd_multiq/run.sh
@@ -9,7 +9,8 @@ k1-jtag-runner --multibinary=pcie_fwd_multiq_multibin.mpk --exec-multibin=IODDR0
 sleep 20
 
 (sudo ping6 -I modp0.0.0.0 -f fe80::de:adff:febe:ef80 -c 8192 -w 10 > /dev/null) && \
-    ping6 -c 10 -w 11 -I modp0.0.0.0 fe80::de:adff:febe:ef80
+    sleep 1 && \
+    ping6 -c 10 -w 12 -I modp0.0.0.0 fe80::de:adff:febe:ef80
 res=$?
 
 # Kill jtag and join it
